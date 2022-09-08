e200a70067d072ca6aef94dca2b9b8140de0359d
loxknew <bangbang12353@gmail.com>
2022-09-09 00:57:13 +0700

    4.3

--- src/main/java/se331/rest/Lab08Application.java
+++ src/main/java/se331/rest/Lab08Application.java
@@ -2,6 +2,9 @@ package se331.rest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Lab08Application {
@@ -9,5 +12,14 @@ public class Lab08Application {
     public static void main(String[] args) {
         SpringApplication.run(Lab08Application.class, args);
     }
-
+    @Bean
+    public WebMvcConfigurer corsConfigurer(){
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings (CorsRegistry registry){
+                registry.addMapping("/event")
+                        .allowedOrigins("http://localhost:3000");
+            }
+        };
+    }
 }
